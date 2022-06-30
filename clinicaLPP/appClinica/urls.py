99ae05a5ef03dabb4c45e64a92b61356d2363bf0
Franco <piconito.f@gmail.com>
2022-06-30 12:09:11 -0300

    Navegacion por barra 1

--- clinicaLPP/appClinica/urls.py
+++ clinicaLPP/appClinica/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from appClinica.views import inicio
+from appClinica.views import inicio,turno,contacto,nosotros,profesionales,admin
 
 urlpatterns = [
-    path('', inicio),
+    path('', inicio, name='inicio'),
+    path('turno/', turno, name='turno'),
+    path('nosotros/', nosotros, name='nosotros'),
+    path('profesionales/', profesionales, name='profesionales'),
+    path('contacto/', contacto, name='contacto'),
+    path('adminpage/', admin, name='admin'),
 ]
--- clinicaLPP/appClinica/views.py
+++ clinicaLPP/appClinica/views.py
@@ -7,3 +7,18 @@ from django.shortcuts import render
 
 def inicio(request):
     return render(request, 'appClinica/inicio.html')
+
+def nosotros(request):
+    return render(request, 'appClinica/nosotros.html')
+
+def profesionales(request):
+    return render(request, 'appClinica/profesionales.html')
+
+def turno(request):
+    return render(request, 'appClinica/turno.html')
+
+def contacto(request):
+    return render(request, 'appClinica/contacto.html')
+
+def admin(request):
+    return render(request, 'appClinica/admin.html')
